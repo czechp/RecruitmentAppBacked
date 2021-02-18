07851e8a16dc219fb582298832bf806df0489c55
PCzech <you@example.com>
2021-02-18 21:07:48 +0100

    Init

---
+++ src/test/java/com/github/czechp/recruitmentapp/RecruitmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.czechp.recruitmentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
