867a8539abf619f001a0c5a39042ee4e90346e6c
PCzech <you@example.com>
2021-02-28 22:00:12 +0100

    Configure AWS 3s Access

---
+++ adapter/src/main/java/com/github/czechp/recruitmentapp/utility/aws/AwsFileStorage.java
@@ -0,0 +1,6 @@
+package com.github.czechp.recruitmentapp.utility.aws;
+
+import com.github.czechp.recruitmentapp.utility.FileStorage;
+
+public class AwsFileStorage implements FileStorage {
+}
---
+++ adapter/src/main/java/com/github/czechp/recruitmentapp/utility/aws/AwsFileStorageConfiguration.java
@@ -0,0 +1,29 @@
+package com.github.czechp.recruitmentapp.utility.aws;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration()
+class AwsFileStorageConfiguration {
+    //TODO: read credentials from environment variables
+    @Bean()
+    AWSCredentials getAwsCredentials(){
+        return new BasicAWSCredentials("AKIAIT2ZNQPKY6WZ5W2Q", "nwZHjucy+TOaevKmKLWYkhtzAe84m0qgTkf8Hi1+");
+    }
+
+    @Bean()
+    AmazonS3 getAmazonS3Client(){
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(getAwsCredentials()))
+                .withRegion(Regions.EU_CENTRAL_1)
+                .build();
+    }
+}
